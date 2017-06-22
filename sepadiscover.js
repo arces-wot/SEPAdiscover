d83ca76a2494e8dfd2757e2b205bacfe03ab9f3a
Fabio Viola <desmovalvo@gmail.com>
2017-06-22 18:15:52 +0200

    Added property monitoring; bugs fixed in discovery mechanism

--- sepadiscover.js
+++ sepadiscover.js
@@ -1,9 +1,11 @@
+// state variables
+var deviceListSub = null;
+var devicePropertySub = null;
+
 function subscribe(){
 
     // read form data
-    queryUrl = document.getElementById("queryURI").value;
     subscUrl = document.getElementById("subscribeURI").value;
-    console.log(subscUrl);
 
     // subscription
     
@@ -12,7 +14,15 @@ function subscribe(){
     
     // 2 - send subscription
     ws.onopen = function(){
-	wsText = "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX td:<http://w3c.github.io/wot/w3c-wot-td-ontology.owl#> SELECT ?s ?n WHERE { ?s rdf:type td:Thing . ?s td:hasName ?n }";
+	wsText = "PREFIX wot:<http://www.arces.unibo.it/wot#> " +
+	    "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+	    "PREFIX td:<http://w3c.github.io/wot/w3c-wot-td-ontology.owl#> "+
+	    "SELECT ?thingUri ?thingName ?thingStatus " +
+	    "WHERE { " +
+	    "?thingUri rdf:type td:Thing . " +
+	    "?thingUri td:hasName ?thingName . " +
+	    "?thingUri wot:isDiscoverable ?thingStatus " +
+	    "}";
 	ws.send(JSON.stringify({"subscribe":wsText, "alias":"-"}));
     };
     
@@ -21,22 +31,29 @@ function subscribe(){
 	
 	// parse the message
 	msg = JSON.parse(event.data);
+	console.log(msg);
 
-	// store the subscription ID
-	if (msg["subscribed"] !== undefined){	   
+	// get and store the subscription ID
+	if (msg["subscribed"] !== undefined){
+
+	    // get the subid
 	    subid = msg["subscribed"];
-	    // document.getElementById("spuid").innerHTML = subid;
+
+	    // store the subid into the html field
+	    document.getElementById("spuid").innerHTML = subid;
+
+	    // store the subscription into the global variable
+	    deviceListSub = ws;
+	    
 	} else if (msg["results"] !== undefined){
-	    console.log(msg["results"]);
-	    console.log(msg["results"]["head"]);
-	    console.log(msg["results"]["addedresults"]);
 
 	    // iterate over rows of the results
-	    for (var i in msg["results"]["addedresults"]){
+	    for (var i in msg["results"]["addedresults"]["bindings"]){
 
 		// iterate over columns
-		turi = msg["results"]["addedresults"][i][0]["s"]["value"];
-		name = msg["results"]["addedresults"][i][0]["n"]["value"];
+		turi = msg["results"]["addedresults"]["bindings"][i]["thingUri"]["value"];
+		name = msg["results"]["addedresults"]["bindings"][i]["thingName"]["value"];
+		status = msg["results"]["addedresults"]["bindings"][i]["thingStatus"]["value"];
 		var table = document.getElementById("deviceTable");
 		var row = table.insertRow(-1);
 		var f1 = row.insertCell(0);
@@ -44,7 +61,13 @@ function subscribe(){
 		var f3 = row.insertCell(2);
 		f1.innerHTML = turi;
 		f2.innerHTML = name;
-		f3.innerHTML = '<span class="glyphicon glyphicon-search" aria-hidden="true"></span>';		
+		if (status === "true"){
+		    f3.innerHTML = '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>';
+		} else {
+		    f3.innerHTML = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+		}
+		var f4 = row.insertCell(3);
+		f4.innerHTML = '<button type="button" class="btn btn-secondary" onclick="javascript:monitor(\'' + turi + '\');"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>';
 	    }
 
 	}
@@ -58,5 +81,73 @@ function subscribe(){
 	console.log(document.getElementById("spuid"));
 	console.log("[INFO] Subscription " + subid + " closed.");
     }; 
+    
+};
+
+function monitor(deviceId){
+
+    console.log(test);
+    
+    // close open subscriptions
+    if (devicePropertySub !== null){
+	console.log("[DEBUG] Closing previous subscription");
+	devicePropertySub.close();
+    }
+    
+    
+    // prepare the subscription
+    subscUrl = document.getElementById("subscribeURI").value;
+    subText = "PREFIX wot:<http://www.arces.unibo.it/wot#> PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX td:<http://w3c.github.io/wot/w3c-wot-td-ontology.owl#> PREFIX dul:<http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#> SELECT ?p ?pName ?pValue WHERE { <" + deviceId + "> td:hasProperty ?p . ?p td:hasName ?pName . ?p td:hasValueType ?pvt . ?pvt dul:hasDataValue ?pValue }";
+
+    // subscription
+    
+    // 1 - open connection
+    var ws2 = new WebSocket(subscUrl);
+    
+    // 2 - send subscription
+    ws2.onopen = function(){
+	ws2.send(JSON.stringify({"subscribe":subText, "alias":"-"}));
+    };
+    
+    // 3 - handler for received messages
+    ws2.onmessage = function(event){
+	
+	// parse the message
+	msg = JSON.parse(event.data);
+	console.log(msg);
+
+	// store the subscription ID
+	if (msg["subscribed"] !== undefined){	   
+	    subid = msg["subscribed"];
+	    document.getElementById("deviceSpuid").innerHTML = subid;
+	} else if (msg["results"] !== undefined){
+
+	    // iterate over rows of the results
+	    for (var i in msg["results"]["addedresults"]["bindings"]){
+		
+		// iterate over columns
+		puri = msg["results"]["addedresults"]["bindings"][i]["p"]["value"];
+		pname = msg["results"]["addedresults"]["bindings"][i]["pName"]["value"];
+		pvalue = msg["results"]["addedresults"]["bindings"][i]["pValue"]["value"];
+		var table = document.getElementById("devicePropTable");
+		var row = table.insertRow(-1);
+		var f1 = row.insertCell(0);
+		var f2 = row.insertCell(1);
+		var f3 = row.insertCell(2);
+		f1.innerHTML = puri;
+		f2.innerHTML = pname;
+		f3.innerHTML = pvalue;
+	    }
+
+	}
+	
+    };
+
+    // 4 - handler for closed websocket
+    ws2.onclose = function(event){
+	subid = document.getElementById("spuid").value;
+	document.getElementById("deviceSpuid").innerHTML = "";	
+    }; 
+
     
 };
